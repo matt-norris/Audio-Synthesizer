ed0df801fefb43558035a1f37ce5599253b0e532
norrism7 <norrism7@RDSVDI-246>
2022-10-29 01:02:26 -0400

    Started second effect, compression

---
+++ Synthie/Compression.cpp
@@ -0,0 +1,22 @@
+#include "pch.h"
+#include "Compression.h"
+
+void CCompression::SetNote(CNote* note)
+{
+
+}
+
+void CCompression::Process(double* frameIn, double* frameOut)
+{
+
+}
+
+CCompression::CCompression(void)
+{
+
+}
+
+CCompression::~CCompression(void)
+{
+
+}
---
+++ Synthie/Compression.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Effects.h"
+class CCompression :
+    public CEffects
+{
+public:
+    //! Start the node generation
+    virtual void Start() {};
+
+    //! Cause one sample to be generated
+    virtual bool Generate() { return false; }
+
+
+    void SetNote(CNote* note);
+
+    virtual void Process(double* frameIn, double* frameOut);
+
+    CCompression(void);
+
+    virtual ~CCompression(void);
+};
+
